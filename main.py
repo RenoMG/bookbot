fec373e9282bd82e69aa16196b0d7f8c1a6ea404
EsperAdmin <reno@renos.world>
2025-12-30 00:33:22 -0800

    remove hard-coded path

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from stats import get_num_words, char_counter, sort_char_count, organise_char_count
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 def main(input):
+    if len(input) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+
+    input = input[1]
+
     get_data = organise_char_count(input)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {input}...")
@@ -19,4 +26,4 @@ def main(input):
     print("============= END ===============")
 
 #print("Please enter a file path for a txt file.")
-main("books/frankenstein.txt")
+main(sys.argv)

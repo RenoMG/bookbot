57c9dffb0f1d54fd412c9805c848c02a0fc7fbee
EsperAdmin <reno@renos.world>
2025-12-29 23:21:35 -0800

    make 2 seperate functions

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def get_book_text(file_path):
     with open(file_path) as f:
+        return f.read()
+
+def get_num_words(file_path):
+    with open(file_path) as f:
         return f"Found {len(f.read().split())} total words"
 
 

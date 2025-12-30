d948dcfea78ebfc900c8e3e1c29bd4a56713ba17
EsperAdmin <reno@renos.world>
2025-12-29 23:37:56 -0800

    add character counter

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from stats import get_num_words
+from stats import get_num_words, char_counter
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 def main(input):
-    print(get_num_words(input))
+    print(char_counter(input))
 
 #print("Please enter a file path for a txt file.")
 main("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -1,3 +1,18 @@
 def get_num_words(file_path):
     with open(file_path) as f:
         return f"Found {len(f.read().split())} total words"
+
+def char_counter(file_path):
+    char_count_db = {}
+
+    with open(file_path) as f:
+        book_text = f.read()
+
+    for char in book_text:
+        if char.lower() not in char_count_db:
+            char_count_db[char.lower()] = 0
+
+        if char.lower() in char_count_db:
+            char_count_db[char.lower()] = char_count_db[char.lower()] + 1
+
+    return char_count_db

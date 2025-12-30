6f908578c3e9585b981240354f8cca828bf6f32a
EsperAdmin <reno@renos.world>
2025-12-29 23:16:34 -0800

    find all words in the book

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f"Found {len(f.read().split())} total words"
+
+
+def main(input):
+    print(get_book_text(input))
+
+#print("Please enter a file path for a txt file.")
+main("books/frankenstein.txt")
